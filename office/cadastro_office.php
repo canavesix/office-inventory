794bf52ab74e462556a3a9cf5be40af05f85a065
Giovanna Cinoca <72810976+ginocax@users.noreply.github.com>
2023-04-27 10:16:00 -0300

    Alteração da estrutura de pastas

--- office/cadastro_office.php
+++ office/cadastro_office.php
@@ -1,88 +1,88 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Inventário</title>
-	<script src="https://kit.fontawesome.com/eb55db7828.js" crossorigin="anonymous"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Inventário</title>
+  <script src="https://kit.fontawesome.com/eb55db7828.js" crossorigin="anonymous"></script>
 
-	<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-	rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-	crossorigin="anonymous">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-	<!-- Custom CSS -->
-	<style>
-		body {
-			background-color: #f7f7f7;
-		}
+  <!-- Custom CSS -->
+  <style>
+    body {
+      background-color: #f7f7f7;
+    }
 
-		.card {
-			margin-bottom: 40px;
-			box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
-			border-radius: 0.25rem;
-			border: none;
-		}
+    .card {
+      margin-bottom: 40px;
+      box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
+      border-radius: 0.25rem;
+      border: none;
+    }
 
-		.card-title {
-			font-size: 1.25rem;
-			font-weight: 600;
-			color: #333;
-		}
+    .card-title {
+      font-size: 1.25rem;
+      font-weight: 600;
+      color: #333;
+    }
 
-		.card-text {
-			font-size: 1rem;
-			color: #666;
-		}
+    .card-text {
+      font-size: 1rem;
+      color: #666;
+    }
 
-		.btn-primary {
-			background-color: #4e73df;
-			border-color: #4e73df;
-		}
+    .btn-primary {
+      background-color: #4e73df;
+      border-color: #4e73df;
+    }
 
-		.btn-primary:hover {
-			background-color: #2e59d9;
-			border-color: #2e59d9;
-		}
+    .btn-primary:hover {
+      background-color: #2e59d9;
+      border-color: #2e59d9;
+    }
 
-		h1 {
-			margin-top: 100px;
-			margin-bottom: 30px;
-			font-size: 2rem;
-			text-align: center;
-			color: #333;
-		}
+    h1 {
+      margin-top: 100px;
+      margin-bottom: 30px;
+      font-size: 2rem;
+      text-align: center;
+      color: #333;
+    }
 
-		/* Additional customizations for visual enhancement */
-		.navbar-brand {
-			font-size: 2rem;
-			color: #4e73df;
-			font-weight: bold;
-		}
+    /* Additional customizations for visual enhancement */
+    .navbar-brand {
+      font-size: 2rem;
+      color: #4e73df;
+      font-weight: bold;
+    }
 
-		.navbar {
-			background-color: #f7f7f7;
-			padding: 20px;
-			background-color: #343a40;
-			box-shadow: 0px 2px 10px rgba(0,0,0,0.3);
-		}
+    .navbar {
+      background-color: #f7f7f7;
+      padding: 20px;
+      background-color: #343a40;
+      box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.3);
+    }
 
-		.nav-link {
-			font-size: 1.25rem;
-			color: white;
-			font-weight: bold;
-			text-transform: uppercase;
-			padding: 10px;
-		}
+    .nav-link {
+      font-size: 1.25rem;
+      color: white;
+      font-weight: bold;
+      text-transform: uppercase;
+      padding: 10px;
+    }
 
-		.nav-link:hover {
-			color: #4e73df;
-		}
-	</style>
+    .nav-link:hover {
+      color: #4e73df;
+    }
+  </style>
 </head>
+
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container-fluid">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
       <a class="navbar-brand" href="https://github.com/canavesix">G.C</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -93,7 +93,7 @@
             <a class="nav-link active" aria-current="page" href=""></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="">Office</a>
+            <a class="nav-link" href="diretores.php">Menu</a>
           </li>
         </ul>
       </div>
@@ -115,70 +115,71 @@
     </div>
     <hr>
 
-    
 
-            <br>
-            <h5>Cadastrar manualmente:</h5>
-            <form action="_inserir_produto.php" method="post" style="margin-top: 20px">
-                <div class="form-group">
-                    <label>Programa</label>
-                    <input type="text" class="form-control" name="nroproduto" placeholder="Insira o número de série do equipamento." autocomplete="off">
-                </div>
 
-                  <div class="form-group">
-                      <label for="nomeproduto">CW:</label>
-                      <input type="text" class="form-control" id="nomeproduto" name="nomeproduto" placeholder="Insira o nome do produto" autocomplete="off" required>
-                    </div>
+    <br>
+    <h5>Cadastrar manualmente:</h5>
+    <form action="_inserir_office.php" method="post" style="margin-top: 20px">
+      <div class="form-group">
+        <label>Programa</label>
+        <input type="text" class="form-control" name="" placeholder="Insira o tipo da licença" autocomplete="off">
+      </div>
 
-                    <div class="form-group">
-                      <label for="categoria">Email:</label>
-                    </div>
+      <div class="form-group">
+        <label for="nomeproduto">CW:</label>
+        <input type="text" class="form-control" id="" name="" placeholder="Insira o número do CW" autocomplete="off" required>
+      </div>
 
-                    <div class="form-group">
-                        <label for="computador"> Computador</label>
-                        <input type="number" class="form-control" name="quantidade" placeholder="Insira quantidade do produto" required>
-                    </div>
+      <div class="form-group">
+        <label for="categoria">Email:</label>
+        <input type="number" class="form-control" name="" placeholder="Insira o e-mail atrelado" required>
+      </div>
 
-                    <div class="form-group">
-                        <label>Setor</label>
-                        <input type="text" class="form-control" name="setor" placeholder="Insira o setor de destino">
-                    </div>
+      <div class="form-group">
+        <label for="computador"> Computador</label>
+        <input type="number" class="form-control" name="hostname" placeholder="Insira o hostname">
+      </div>
 
-                    <div class="form-group">
-                        <label>Tipo</label>
-                        <input type="text" class="form-control" name="setor" placeholder="Insira o setor de destino">
-                    </div>
+      <div class="form-group">
+        <label>Setor</label>
+        <input type="text" class="form-control" name="setor" placeholder="Insira o setor atrelado a licença">
+      </div>
 
-                    <div class="form-group">
-                        <label>Centro Custo</label>
-                        <input type="text" class="form-control" name="setor" placeholder="Insira o setor de destino">
-                    </div>
+      <div class="form-group">
+        <label>Tipo</label>
+        <input type="text" class="form-control" name="" placeholder="Callcenter/Adm">
+      </div>
 
-                    <div class="form-group">
-                        <label>SU</label>
-                        <input type="text" class="form-control" name="setor" placeholder="Insira o setor de destino">
-                    </div>
+      <div class="form-group">
+        <label>Centro Custo</label>
+        <input type="text" class="form-control" name="" placeholder="Insira o Centro de Custo do Setor">
+      </div>
 
-                    <div class="form-group">
-                        <label>Nome</label>
-                        <input type="text" class="form-control" name="setor" placeholder="Insira o setor de destino">
-                    </div>
+      <div class="form-group">
+        <label>SU</label>
+        <input type="text" class="form-control" name="" placeholder="Insira o número da SU">
+      </div>
 
-                    <div class="form-group">
-                        <label>Status</label>
-                        <input type="text" class="form-control" name="setor" placeholder="Insira o setor de destino">
-                    </div>
-                    
+      <div class="form-group">
+        <label>Nome</label>
+        <input type="text" class="form-control" name="setor" placeholder="Insira o nome do usuário atrelado a licença">
+      </div>
 
-   <br>
+      <div class="form-group">
+        <label>Status</label>
+        <input type="text" class="form-control" name="" placeholder="">
+      </div>
+
+
+      <br>
       <button type="submit" id="botao" class="btn btn-primary" style="color: #fff;margin-right: 12px">Cadastrar</button>
       <a href="menu.php" role="button" class="btn btn-light">Voltar</a>
-   </div>
-</div>
+  </div>
+  </div>
 
-        <script type="text/javascript" src="js/bootstrap.js"></script>
-        
+  <script type="text/javascript" src="js/bootstrap.js"></script>
 
-    </body>
 
-    </html>
+</body>
+
+</html>
--- office/index.php
+++ office/index.php
@@ -1,91 +1,86 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Login - Sistema de Cadastro de Serviços Office 365</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- CSS do Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-    crossorigin="anonymous">
-    
-    <!-- CSS customizado -->
-    <style>
-  body {
-    background-color: #f0f0f0;
-    font-family: Arial, sans-serif;
-  }
-  
-  .card {
-    background-color: #ffffff;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    transition: 0.3s;
-  }
-  
-  .card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-  }
-  
-  .form-control {
-    border-radius: 0;
-  }
-  
-  .btn-primary {
-    background-color: #007bff;
-    border: none;
-    transition: all 0.2s;
-  }
-  
-  .btn-primary:hover {
-    background-color: #0069d9;
-  }
-  
-  .btn-block {
-    border-radius: 0;
-  }
-  
-  h1 {
-    font-size: 36px;
-    text-align: center;
-    margin-top: 50px;
-  }
-</style>
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <div class="container">
-      <div class="row justify-content-center mt-5">
-        <div class="col-md-6 col-lg-4">
-          <div class="card">
-            <div class="card-body">
-              <h3 class="text-center">Login</h3>
-              <form>
-                <div class="form-group">
-                  <label for="email">E-mail</label>
-                  <input type="email" class="form-control" id="email" name="email" required>
-                </div>
-                <div class="form-group">
-                  <label for="password">Senha</label>
-                  <input type="password" class="form-control" id="password" name="password" required>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Entrar</button>
-              </form>
-            </div>
+
+<head>
+  <title>Sistema de Cadastro de Serviços Office 365</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- CSS do Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- CSS customizado -->
+  <style>
+    body {
+      background-color: #f0f0f0;
+      font-family: Arial, sans-serif;
+    }
+
+    .card {
+      background-color: #ffffff;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      transition: 0.3s;
+    }
+
+    .card:hover {
+      box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    }
+
+    .form-control {
+      border-radius: 0;
+    }
+
+    .btn-primary {
+      background-color: #007bff;
+      border: none;
+      transition: all 0.2s;
+    }
+
+    .btn-primary:hover {
+      background-color: #0069d9;
+    }
+
+    .btn-block {
+      border-radius: 0;
+    }
+
+    h1 {
+      font-size: 36px;
+      text-align: center;
+      margin-top: 50px;
+    }
+  </style>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="row justify-content-center mt-5">
+      <div class="col-md-6 col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h3 class="text-center">Login</h3>
+            <form>
+              <div class="form-group">
+                <label for="email">E-mail</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+              </div>
+              <div class="form-group">
+                <label for="password">Senha</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+              </div>
+              <button type="submit" class="btn btn-primary btn-block">Entrar</button>
+            </form>
           </div>
         </div>
       </div>
     </div>
-    
-    <!-- Scripts do Bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi4+N" 
-    crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
-    crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
-    crossorigin="anonymous"></script>
-  </body>
+  </div>
+
+  <!-- Scripts do Bootstrap -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi4+N" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
 </html>
